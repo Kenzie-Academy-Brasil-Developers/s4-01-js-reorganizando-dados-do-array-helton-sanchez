24ddd959e6b122b9e2cf7620fdf0baf40b0bf4b7
Helton Sanchez <114593802+Heltonhs@users.noreply.github.com>
2022-12-14 15:03:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//Desenvolva sua lógica aqui utilizando o método de array sort
+
+//1) Dado o array [200, 153, 875, 53, 20, 62, 428, 5], ordene os números em ordem crescente.
+const numbers = [200, 153, 875, 53, 20, 62, 428, 5];
+
+//2) Utilizando o array do exemplo anterior, ordene os números em ordem decrescente.
+
+//3) Dado o array ['Matheus', 'Samuel', 'Bruna', 'Patrick', 'Yan', 'Amanda', 'Carlos'], ordene os nomes em ordem alfabética
+const names = [
+  'Matheus',
+  'Samuel',
+  'Bruna',
+  'Patrick',
+  'Yan',
+  'Amanda',
+  'Carlos',
+];
+
+//DESAFIO
+//4) Dado um objeto "loja", acesse a propriedade "produtos", filtre apenas os elementos que possuem o preço maior que 100 e ordene em ordem crescente.
+const loja = {
+  nome: 'Kenzie Store',
+  local: 'Brasil',
+  produtos: [
+    {
+      nomeProduto: 'Moletom',
+      preco: 250,
+    },
+    {
+      nomeProduto: 'Casaco',
+      preco: 100,
+    },
+    {
+      nomeProduto: 'Tênis',
+      preco: 180,
+    },
+    {
+      nomeProduto: 'Regata',
+      preco: 50,
+    },
+    {
+      nomeProduto: 'Calça',
+      preco: 120,
+    },
+  ],
+};
